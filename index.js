c633144e1ea14feeafe4810fcb1c27ec819dce2b
NdekoCode <arickbulakali@gmail.com>
2022-08-15 15:13:38 +0200

    add error and keyup event on submitting

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const langues = ["sw", "fr", "ln", "en"];
 const blockSearch = document.getElementById("block-search");
 const valueContainer = document.getElementById("search-input");
 
-let loading = true;
 const fetchData = async (value) => {
   const url = `https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${value}&limit=15`;
   const response = await fetch(url);
@@ -13,10 +12,11 @@ const fetchData = async (value) => {
     console.log(responseData);
     const [contentSearch, titles, contents, Urls] = responseData;
     const sectionHTML = document.createElement("section");
-    titles.forEach((title, index) => {
-      const card = `
+    if (titles.length > 0) {
+      titles.forEach((title, index) => {
+        const card = `
             <div class="card mt-5">
-                <a href="#" class="text-gray-800">${Urls[index]}</a>
+                <a href="${Urls[index]}" class="text-gray-800">${Urls[index]}</a>
                 <h3 class="text-blue-800 mt-3">
                     <a href="${Urls[index]}">${title}</a></h3>
                 <p class="text-gray-500">f you'd like to receive messages about upcoming talks, please subscribe to the
@@ -24,27 +24,49 @@ const fetchData = async (value) => {
                     sending an email to dsdsd-list-subscribe@cwi.nl.
                 </p>
             </div>`;
-      sectionHTML.innerHTML += card;
-    });
+        sectionHTML.innerHTML += card;
+      });
+    } else {
+      sectionHTML.innerHTML = `
+            <div class="card mt-5">
+                <p class="text-gray-500">
+                Aucune donnée trouver pour votre recherche
+                </p>
+            </div>`;
+    }
     blockSearch.innerHTML = "";
     blockSearch.append(sectionHTML);
   }
 };
-btnSearch.addEventListener("click", function (evt) {
-  evt.preventDefault();
 
+function loadDataSearch() {
+  let loading = true;
   if (loading) {
     const loader = `
-      <div className="loadingContainer">
-        <div className="loading">
-          <span className="loadWords">Loading...</span>
-          <span className="loading__anim"></span>
+      <div class="loadingContainer">
+        <div class="loading">
+          <span class="loadWords">Loading...</span>
+          <span class="loading__anim"></span>
         </div>
       </div>`;
 
     blockSearch.innerHTML = loader;
-    loading = false;
   }
   const search = valueContainer.value;
-  fetchData(search);
+  if (search.length > 0 && (search !== "" || search !== null)) {
+    fetchData(search);
+  }
+}
+btnSearch.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log(evt.code);
+  loadDataSearch();
+});
+
+valueContainer.addEventListener("keyup", function (evt) {
+  evt.preventDefault();
+
+  if (evt.code == "Enter") {
+    loadDataSearch();
+  }
 });
